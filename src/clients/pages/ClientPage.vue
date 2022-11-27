a59afeca6da4e329010d16fd177af5c86c2e90f8
LcrPrograming <83461083+Luiscrd@users.noreply.github.com>
2022-11-27 15:51:08 +0100

    update

--- src/clients/pages/ClientPage.vue
+++ src/clients/pages/ClientPage.vue
@@ -17,7 +17,7 @@ watch(isError, () => {
         setTimeout(() => {
 
             router.replace('/client/list');
-            
+
         }, 3000);
     }
 
@@ -37,45 +37,45 @@ const goTo = () => {
     <img v-if="client" :src="client.picture" :alt="client.name">
     <form v-if="client" @submit.prevent="clientMutation.mutate(client!)">
         <div class="mb-3 row">
-            <label for="inputPassword" class="col-sm-2 col-form-label">Nombre</label>
+            <label class="col-sm-2 col-form-label">Nombre</label>
             <div class="col-sm-10">
                 <input type="text" class="form-control" v-model="client.name" placeholder="Introduce Nombre...">
             </div>
         </div>
         <div class="mb-3 row">
-            <label for="inputPassword" class="col-sm-2 col-form-label">Dirección</label>
+            <label class="col-sm-2 col-form-label">Dirección</label>
             <div class="col-sm-10">
                 <input type="text" class="form-control" v-model="client.address" placeholder="Introduce Dirección...">
             </div>
         </div>
         <div class="mb-3 row">
-            <label for="inputPassword" class="col-sm-2 col-form-label">Email</label>
+            <label class="col-sm-2 col-form-label">Email</label>
             <div class="col-sm-10">
                 <input type="email" class="form-control" v-model="client.email" placeholder="Introduce Email...">
             </div>
         </div>
         <div class="mb-3 row">
-            <label for="inputPassword" class="col-sm-2 col-form-label">Teléfono</label>
+            <label class="col-sm-2 col-form-label">Teléfono</label>
             <div class="col-sm-10">
                 <input type="text" class="form-control" v-model="client.phone" placeholder="Introduce Teléfono...">
             </div>
         </div>
         <div class="mb-3 row">
-            <label for="inputPassword" class="col-sm-2 col-form-label">Compañia</label>
+            <label class="col-sm-2 col-form-label">Compañia</label>
             <div class="col-sm-10">
                 <input type="text" class="form-control" v-model="client.company" placeholder="Introduce compañia...">
             </div>
         </div>
         <div class="mb-3 row">
-            <label for="inputPassword" class="col-sm-2 col-form-label">Edad</label>
+            <label class="col-sm-2 col-form-label">Edad</label>
             <div class="col-sm-10">
                 <input type="number" class="form-control" v-model="client.age" min="0" placeholder="Introduce Edad...">
             </div>
         </div>
         <div class="mb-3 row">
-            <label for="inputPassword" class="col-sm-2 col-form-label">Genero</label>
+            <label class="col-sm-2 col-form-label">Genero</label>
             <div class="col-sm-10">
-                <select class="form-select" v-model="client.gender" aria-label="Default select example">
+                <select class="form-select" v-model="client.gender">
                     <option selected>Selecciona un Genero</option>
                     <option value="masculino">Masculino</option>
                     <option value="femenino">Femenino</option>
@@ -84,9 +84,9 @@ const goTo = () => {
             </div>
         </div>
         <div class="mb-3 row">
-            <label for="inputPassword" class="col-sm-2 col-form-label">C. Ojos</label>
+            <label class="col-sm-2 col-form-label">C. Ojos</label>
             <div class="col-sm-10">
-                <select class="form-select" v-model="client.eyeColor" aria-label="Default select example">
+                <select class="form-select" v-model="client.eyeColor">
                     <option selected>Selecciona un color</option>
                     <option value="verde">Verde</option>
                     <option value="marron">Marron</option>
@@ -95,16 +95,22 @@ const goTo = () => {
             </div>
         </div>
         <div class="mb-3 row">
-            <label for="inputPassword" class="col-sm-2 col-form-label">Imagen</label>
+            <label class="col-sm-2 col-form-label">Estado</label>
             <div class="col-sm-10">
-                <div class="input-group mb-3">
-                    <input type="text" class="form-control" v-model="client.picture"
-                        placeholder="Introduce url valido...">
-                    <button class="btn btn-outline-warning" type="button" id="button-addon2">Validar</button>
-                </div>
+                <select class="form-select" v-model="client.isActive">
+                    <option selected>Selecciona un Estado</option>
+                    <option :value="true">Activo</option>
+                    <option :value="false">Inactivo</option>
+                </select>
             </div>
         </div>
-        <button type="submit" class="btn btn-outline-warning save"
+        <div class="mb-3 row">
+            <label class="col-sm-2 col-form-label">Imagen</label>
+            <div class="col-sm-10">
+                <input type="text" class="form-control" v-model="client.picture" placeholder="Introduce url valido...">
+            </div>
+        </div>
+        <button type="submit" class="btn btn-outline-success save"
             :disabled="clientMutation.isLoading.value">Guaradr</button>
         <button type="submit" class="btn btn-outline-warning return" @click="goTo()">Volver</button>
     </form>
