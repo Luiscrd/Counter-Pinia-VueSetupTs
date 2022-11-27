81b0f1eaeac79d652e9081c02b1ee1ef43ac5123
LcrPrograming <83461083+Luiscrd@users.noreply.github.com>
2022-11-27 06:32:49 +0100

    upload

---
+++ src/clients/components/ClientList.vue
@@ -0,0 +1,71 @@
+<script setup lang='ts'>
+import { useRouter } from 'vue-router';
+
+
+const router = useRouter();
+
+const goTo = (id: number) => {
+
+    router.push(`/client/${id}`);
+}
+
+</script>
+
+<template>
+    <table class="table">
+        <thead>
+            <tr>
+                <th scope="col">Id</th>
+                <th scope="col">Foto</th>
+                <th scope="col">Nombre</th>
+                <th scope="col">Estado</th>
+            </tr>
+        </thead>
+        <tbody>
+            <tr @click="goTo(1)">
+                <th scope="row">1</th>
+                <td>
+                    <img src="https://cdn.iconscout.com/icon/free/png-128/avatar-380-456332.png" alt="Avatar">
+                </td>
+                <td>Donovan Bernardo</td>
+                <td>
+                    <span class="badge text-bg-danger">Inactivo</span>
+                    <!-- <span class="badge text-bg-success">Activo</span> -->
+                </td>
+            </tr>
+            <tr>
+                <th scope="row">2</th>
+                <td>
+                    <img src="https://cdn.iconscout.com/icon/free/png-128/avatar-366-456318.png" alt="Avatar">
+                </td>
+                <td>Mcclure Rollins</td>
+                <td>
+                    <!-- <span class="badge text-bg-danger">Inactivo</span> -->
+                    <span class="badge text-bg-success">Activo</span>
+                </td>
+            </tr>
+        </tbody>
+    </table>
+</template>
+
+<style scoped>
+table {
+    color: gray;
+    vertical-align: middle;
+}
+
+img {
+    width: 50px;
+    border-radius: 100%;
+}
+
+tbody tr {
+    cursor: pointer;
+}
+
+tbody tr:hover {
+    transition: 0.3s;
+    background: rgb(49, 49, 49);
+    color: rgb(255, 255, 255);
+}
+</style>
