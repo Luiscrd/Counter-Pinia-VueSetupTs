ef8b33ebce0b472fbd9cef05d3cac3cf4772b323
LcrPrograming <83461083+Luiscrd@users.noreply.github.com>
2022-11-27 03:46:24 +0100

    upload

---
+++ src/counter/components/CounterSetupValue.vue
@@ -0,0 +1,23 @@
+<script setup lang="ts">
+import { storeToRefs } from 'pinia';
+import { useCounterSetupStore } from '@/store/counter-setup';
+
+const counterOptions = useCounterSetupStore();
+
+const { lastChange } = storeToRefs(counterOptions);
+
+</script>
+
+<template>
+    <div>
+        <hr>
+        <h5>Ultímo cambio: {{ lastChange }}</h5>
+        <hr>
+        <a href="https://pinia.vuejs.org/core-concepts/#setup-stores" target="_blank">Pagina Oficial de Pinia</a>
+        <hr>
+    </div>
+</template>
+
+<style scoped>
+
+</style>
--- src/counter/pages/CounterSetupPage.vue
+++ src/counter/pages/CounterSetupPage.vue
@@ -1,11 +1,42 @@
 <script setup lang="ts">
+import { storeToRefs } from 'pinia';
+import { ref } from 'vue';
+import { useCounterSetupStore } from '@/store/counter-setup';
+import CounterSetupValue from '@/counter/components/CounterSetupValue.vue';
+
+const counterOptions = useCounterSetupStore();
+
+const { count, squareCount } = storeToRefs(counterOptions);
+
+const value = ref<number>(2);
+
+const { increment, incrementBy } = counterOptions;
 
 </script>
 
 <template>
-    <h1>Counter 2 Page</h1>
+    <div class="text-center mt-4">
+        <h1>Counter Setup Pinia</h1>
+        <hr>
+        <h2 class="mb-2">Contador: {{ count }}</h2>
+        <h2 class="mb-4">Contador ² : {{ squareCount }}</h2>
+        <div class="input-group mb-3">
+            <button class="btn btn-outline-warning" type="button" @click="increment()">Incrementar + 1</button>
+            <input type="number" class="form-control" placeholder="Introduce cantidad" v-model="value">
+            <button class="btn btn-outline-warning" type="button" @click="incrementBy(value)">Incrementar + {{ value
+            }}</button>
+        </div>
+        <button type="button" class="btn btn-outline-warning btn-reset" @click="counterOptions.$reset()">Reset</button>
+        <CounterSetupValue />
+    </div>
 </template>
 
 <style scoped>
+input {
+    background-color: #d9d9d9;
+}
 
+.btn-reset {
+    width: 100%;
+}
 </style>
