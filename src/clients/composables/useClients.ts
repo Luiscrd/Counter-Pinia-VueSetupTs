febca74c2e4776e8bccdc8942263d34f3e0f0b98
LcrPrograming <83461083+Luiscrd@users.noreply.github.com>
2022-11-27 11:18:24 +0100

    Upload

--- src/clients/composables/useClients.ts
+++ src/clients/composables/useClients.ts
@@ -6,10 +6,6 @@ import { storeToRefs } from 'pinia';
 import { watch } from 'vue';
 
 const getClients = async (page: number): Promise<Client[]> => {
-
-    // await new Promise(resolve => {
-    //     setTimeout(() => resolve(true), 1500);
-    // })
     
     const { data } = await clientApi.get<Client[]>(`/clients?_page=${page}`);
     return data;
